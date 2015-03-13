0a45fe1a76e2d64b1ab5204f756e90ee7bf4beaf
jgillick <j_gillick@yahoo.com>
2015-03-12 21:59:50 -0700

    Turn off the SASS watcher and compile once on load. Otherwise the watcher does not shutdown when the app closes

--- disco-floor-controller/app.js
+++ disco-floor-controller/app.js
@@ -1,18 +1,16 @@
-
+'use strict';
 
 // Compile SASS files
-var sys = require('sys'),
-	path = require('path'),
-	exec = require('child_process').exec;
+var exec = require('child_process').exec;
 
 
 (function compileSass() {
 	
-	exec('sass --watch scss:public/stylesheets', function (error, stdout, stderr) {
-		sys.print('stdout: ' + stdout);
-		sys.print('stderr: ' + stderr);
+	var sassCompiler = exec('sass -I ./ --update ./scss:./public/stylesheets', function (error, stdout, stderr) {
+		if (stdout != '') console.log('SASS: ' + stdout);
+		if (stderr != '') console.log('SASS ERROR: ' + stderr);
 		if (error !== null) {
-			console.log('exec error: ' + error);
+			console.log('SASS exec error: ' + error);
 		}
 	});
 
