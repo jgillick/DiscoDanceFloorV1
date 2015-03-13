0a2c3167aed9dec4bb12ba3ee32055bb58ba9baf
jgillick <j_gillick@yahoo.com>
2015-03-12 22:01:20 -0700

    Add serial setup dialog

--- disco-floor-controller/javascript/preview.js
+++ disco-floor-controller/javascript/preview.js
@@ -1,9 +1,18 @@
-var disco = require('./lib/disco_controller.js');
+'use strict';
+
+var disco = require('./lib/disco_controller.js'),
+	comm = require('./lib/comm.js'),
+	serialPort = require('serialport');
 
 var controller = disco.controller;
 
+/*
+	Init the page
+*/
 $(document).ready(function(){
-	
+	serialSetup();
+
+
 	// Load program list
 	disco.getProgramList()
 	.then(function(programs){
@@ -17,7 +26,7 @@ $(document).ready(function(){
 			item.text = program.name;
 
 			list.append(item);
-		})
+		});
 	});
 
 	// Run program when selected
@@ -26,6 +35,40 @@ $(document).ready(function(){
 			file = list.options[list.selectedIndex].value;
 
 		disco.runProgram(file);
-	})
+	});
 
-})
+});
+
+/**
+	The Serial Communication setup dialog
+*/
+function serialSetup(){
+	var lastPortNum = 0;
+
+	// Add serial ports to the list
+	function updatePorts(){
+		serialPort.list(function (err, ports) {
+			var list = $('#serial-ports-list');
+
+			if (ports.length != lastPortNum) {
+				lastPortNum = ports.length;
+
+				list.empty();
+			  ports.forEach(function(port) {
+			  	list.append('<option>'+ port.comName +'</option>');
+			  });
+			}
+		});
+
+		// Poll
+		setTimeout(updatePorts, 1000);
+	}
+	updatePorts();
+
+	// Port selected, connect and get node count
+	$('#serial-setup .connect').click(function(){
+		var port = $('#serial-ports-list').val();
+		console.log('Open', port);
+		comm.start(port);
+	});
+}
