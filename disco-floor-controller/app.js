67d16319baf489b3ecac0a664c1bf25efa36db0d
jgillick <j_gillick@yahoo.com>
2015-03-15 22:52:10 -0700

    Fix errors for terminal output

--- disco-floor-controller/app.js
+++ disco-floor-controller/app.js
@@ -5,13 +5,15 @@ var exec = require('child_process').exec;
 
 
 (function compileSass() {
-	
+
 	var sassCompiler = exec('sass -I ./ --update ./scss:./public/stylesheets', function (error, stdout, stderr) {
-		if (stdout != '') console.log('SASS: ' + stdout);
-		if (stderr != '') console.log('SASS ERROR: ' + stderr);
+		if (stdout !== '') console.log('SASS: ' + stdout);
+		if (stderr !== '') console.log('SASS ERROR: ' + stderr);
 		if (error !== null) {
 			console.log('SASS exec error: ' + error);
 		}
+
+		// TODO: Refresh CSS files in the DOM
 	});
 
 })();
