8d1b443e6a9d3201053f511449769417d87209d3
jgillick <j_gillick@yahoo.com>
2015-07-26 20:05:17 -0700

    Floor cells should start out black -- managing making them white when off doesn't work well. And tabs to spaces.

--- Disco Controller/lib/emulated_floor.js
+++ Disco Controller/lib/emulated_floor.js
@@ -1,223 +1,202 @@
 'use strict';
 
+/* global process */
+
 var disco = require('./lib/disco_controller.js'),
-		utils = require('./lib/utils.js');
+    utils = require('./lib/utils.js');
 
 (function() {
-	var fadeProcessing = false;
+  var fadeProcessing = false;
 
-	$(document).ready(function(){
-		var dimensions = disco.controller.getDimensions();
+  $(document).ready(function(){
+    var dimensions = disco.controller.getDimensions();
 
-		// Set floor dimensions
-		$('#floor-max-x').val(dimensions.x);
-		$('#floor-max-y').val(dimensions.y);
+    // Set floor dimensions
+    $('#floor-max-x').val(dimensions.x);
+    $('#floor-max-y').val(dimensions.y);
 
-		// User updated floor dimensions
-		$('.dimensions input').keyup(function(){
-			var x = parseInt($('#floor-max-x').val() ),
-				y = parseInt($('#floor-max-y').val() );
+    // User updated floor dimensions
+    $('.dimensions input').keyup(function(){
+      var x = parseInt($('#floor-max-x').val() ),
+        y = parseInt($('#floor-max-y').val() );
 
-			if (x != NaN && y != NaN) {
-				disco.controller.setDimensions(x, y);
-			}
-		});
+      if (!isNaN(x) && !isNaN(y)) {
+        disco.controller.setDimensions(x, y);
+      }
+    });
 
-		// Step on/off floor cells
-		$('table.grid').mousedown(function(evt){
-			if (evt.target.nodeName != "TD") return;
+    // Step on/off floor cells
+    $('table.grid').mousedown(function(evt){
+      if (evt.target.nodeName != 'TD') return;
 
-			var td = $(evt.target),
-				x = parseInt(td.attr('data-x')),
-				y = parseInt(td.attr('data-y')),
-				cell = disco.controller.getCell(x, y);
+      var td = $(evt.target),
+        x = parseInt(td.attr('data-x')),
+        y = parseInt(td.attr('data-y')),
+        cell = disco.controller.getCell(x, y);
 
-			cell.setValue(1);
-		});
-		$('table.grid').mouseup(function(evt){
-			if (evt.target.nodeName != "TD") return;
+      cell.setValue(1);
+    });
+    $('table.grid').mouseup(function(evt){
+      if (evt.target.nodeName != 'TD') return;
 
-			var td = $(evt.target),
-				x = parseInt(td.attr('data-x')),
-				y = parseInt(td.attr('data-y')),
-				cell = disco.controller.getCell(x, y);
+      var td = $(evt.target),
+        x = parseInt(td.attr('data-x')),
+        y = parseInt(td.attr('data-y')),
+        cell = disco.controller.getCell(x, y);
 
-			cell.setValue(0);
-		});
-
-
-		buildFloor(dimensions['x'], dimensions['y']);
-	});
-	window.onresize = sizeTable;
-
-	/**
-		Build floor grid
-	*/
-	function buildFloor(xMax, yMax) {
-		var emulator = $('.emulator'),
-			table = emulator.find('table.grid'),
-			tbody = document.createElement('tbody'),
-			styles = [],
-			tr, td, cell;
-
-		table.empty();
-
-		// Create rows and cells
-		for (var y = 0; y < yMax; y++) {
-			tr = document.createElement('tr');
-			for (var x = 0; x < xMax; x++) {
-				td = document.createElement('td');
-				td.id = "cell-"+ x +"-"+ y;
-				td.setAttribute('data-x', x);
-				td.setAttribute('data-y', y);
-				tr.appendChild(td);
-			}
-			tbody.appendChild(tr);
-		}
-
-		table.append(tbody);
-		process.nextTick(sizeTable);
-	}
-
-	/**
-		Size the table to keep the floor cells square in the available space
-	*/
-	function sizeTable() {
-		var emulator = $('.emulator'),
-				table = emulator.find('.grid'),
-				dimensions = disco.controller.getDimensions(),
-				xMax = dimensions.x,
-				yMax = dimensions.y;
-
-		table.css({
-			'height': '100%',
-			'width': '100%',
-		});
-
-		if (xMax < yMax) {
-			table.css('width', table.outerHeight());
-		}
-		else if (xMax > yMax) {
-			table.css('height', table.outerWidth());
-		}
-	}
+      cell.setValue(0);
+    });
 
 
-	/**
-		Converts an RGB color value to HSL. Conversion formula
-		adapted from http://en.wikipedia.org/wiki/HSL_color_space.
-		Assumes r, g, and b are contained in the set [0, 255] and
-		returns h, s, and l in the set [0, 1].
+    buildFloor(dimensions.x, dimensions.y);
+  });
+  window.onresize = sizeTable;
 
-		SOURCE: http://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
+  /**
+    Build floor grid
+  */
+  function buildFloor(xMax, yMax) {
+    var emulator = $('.emulator'),
+      table = emulator.find('table.grid'),
+      tbody = document.createElement('tbody'),
+      tr, td;
 
-		@param {Array} rgb RGB color values array
-		@return {Array} The HSL representation
-	 */
-	function rgbToHsl(rgb){
-	    var r = rgb[0] / 255,
-	        g = rgb[1] / 255,
-	        b = rgb[2] / 255;
+    table.empty();
 
-	    var max = Math.max(r, g, b), min = Math.min(r, g, b);
-		var h, s, l = (max + min) / 2;
+    // Create rows and cells
+    for (var y = 0; y < yMax; y++) {
+      tr = document.createElement('tr');
+      for (var x = 0; x < xMax; x++) {
+        td = document.createElement('td');
+        td.id = 'cell-'+ x +'-'+ y;
+        td.setAttribute('data-x', x);
+        td.setAttribute('data-y', y);
+        tr.appendChild(td);
+      }
+      tbody.appendChild(tr);
+    }
 
-		if(max == min){
-		  h = s = 0; // achromatic
-		} else {
-		  var d = max - min;
-		  s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-		  switch(max){
-		      case r: h = (g - b) / d ; break;
-		      case g: h = 2 + ( (b - r) / d); break;
-		      case b: h = 4 + ( (r - g) / d); break;
-		  }
-		  h*=60;
-		  if (h < 0) {
-		      h +=360;
-		  }
-		}
+    table.append(tbody);
+    process.nextTick(sizeTable);
+  }
 
-	    return [h, s, l];
-	}
+  /**
+    Size the table to keep the floor cells square in the available space
+  */
+  function sizeTable() {
+    var emulator = $('.emulator'),
+        table = emulator.find('.grid'),
+        dimensions = disco.controller.getDimensions(),
+        xMax = dimensions.x,
+        yMax = dimensions.y;
 
-	/**
-		Get rid of dark colors.
-		The floor has a white filter on it, so when the LEDs
-		are off (i.e. #000000), the floor will be off-white, not black.
+    table.css({
+      'height': '100%',
+      'width': '100%',
+    });
 
-		To do this, convert RGB to HSL and use the lightness value to determine the opacity of
-		the color.
+    if (xMax < yMax) {
+      table.css('width', table.outerHeight());
+    }
+    else if (xMax > yMax) {
+      table.css('height', table.outerWidth());
+    }
+  }
 
-		@param {String or Array} color A HEX string or array of RGB integer values
-		@return {Array} Array of normalized RGB values
-	*/
-	function normalizeColors(color) {
-		var hsla = rgbToHsl(color);
 
-	  // Adjust opacity based on lightness
-	  // Anything below 30% should become more transparent because the
-	  // floor tiles are white
-	  hsla[3] = 1;
-	  if (hsla[2] < 0.3) {
-	    hsla[3] = (hsla[2] / 0.3) + 0.05;
-	  }
+  /**
+    Converts an RGB color value to HSL. Conversion formula
+    adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+    Assumes r, g, and b are contained in the set [0, 255] and
+    returns h, s, and l in the set [0, 1].
 
-	  // Convert to percent
-	  hsla[1] = Math.round(hsla[1] * 100) +'%';
-	  hsla[2] = Math.round(hsla[2] * 100) +'%';
+    SOURCE: http://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
 
-	  return hsla;
-	}
+    @param {Array} rgb RGB color values array
+    @return {Array} The HSL representation
+   */
+  function rgbToHsl(rgb){
+      var r = rgb[0] / 255,
+          g = rgb[1] / 255,
+          b = rgb[2] / 255;
 
-	/**
-		Update the floor as a one frame
-	*/
-	function updateFrame(){
-		var styleEl = document.getElementById('grid-frame'),
-			styles = [],
-			cells = disco.controller.getCells(),
-			color, cell;
+      var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
 
-		for (var i = 0, len = cells.length; i < len; i++) {
-			cell = cells[i];
-			color = normalizeColors(cell.getColor());
-			styles[i] = '#cell-'+ cell.getX() +'-'+ cell.getY() +' { background-color: hsla('+ color.join(',') +') !important; }';
-		}
+    if(max == min){
+      h = s = 0; // achromatic
+    } else {
+      var d = max - min;
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+      switch(max){
+          case r: h = (g - b) / d ; break;
+          case g: h = 2 + ( (b - r) / d); break;
+          case b: h = 4 + ( (r - g) / d); break;
+      }
+      h*=60;
+      if (h < 0) {
+          h +=360;
+      }
+    }
 
-		// Add styles
-		if (styleEl) {
-			styleEl.innerHTML = styles.join('\n');
-		}
-		//window.requestAnimationFrame(updateFrame);)
-	}
+      return [h, s, l];
+  }
 
-	// Update floor grid
-	disco.controller.events.on('dimensions.changed', function(xMax, yMax){
-		buildFloor(xMax, yMax);
-	});
+  /**
+    Get rid of dark colors.
+    The floor has a white filter on it, so when the LEDs
+    are off (i.e. #000000), the floor will be off-white, not black.
 
-	// Start stop fade processing
-	disco.controller.events.on('fadeFrame.start', function(){
-		fadeProcessing = true;
-	});
-	disco.controller.events.on('fadeFrame.end', function(){
-		fadeProcessing = false;
-		// window.requestAnimationFrame(updateFrame);
-	});
+    To do this, convert RGB to HSL and use the lightness value to determine the opacity of
+    the color.
 
-	// Update single floor cell
-	disco.controller.events.on('cell.colorChanged', function emulatorSetColor(x, y, color){
-		var el = document.getElementById('cell-'+ x +'-'+ y);
+    @param {String or Array} color A HEX string or array of RGB integer values
+    @return {Array} Array of normalized RGB values
+  */
+  function normalizeColors(color) {
+    var hsla = rgbToHsl(color);
 
-		if (!el) {
-			return;
-		}
+    // Adjust opacity based on lightness
+    // Anything below 30% should become more transparent because the
+    // floor tiles are white
+    hsla[3] = 1;
+    if (hsla[2] < 0.3) {
+      hsla[3] = (hsla[2] / 0.3) + 0.05;
+    }
 
-		// Get rid of dark colors (because the floor is white)
-		color = normalizeColors(color);
+    // Convert to percent
+    hsla[1] = Math.round(hsla[1] * 100) +'%';
+    hsla[2] = Math.round(hsla[2] * 100) +'%';
 
-		// Set color
-		el.style.background = 'hsla('+ color.join(',') +')';
-	});
+    return hsla;
+  }
+
+  // Update floor grid
+  disco.controller.events.on('dimensions.changed', function(xMax, yMax){
+    buildFloor(xMax, yMax);
+  });
+
+  // Start stop fade processing
+  disco.controller.events.on('fadeFrame.start', function(){
+    fadeProcessing = true;
+  });
+  disco.controller.events.on('fadeFrame.end', function(){
+    fadeProcessing = false;
+    // window.requestAnimationFrame(updateFrame);
+  });
+
+  // Update single floor cell
+  disco.controller.events.on('cell.colorChanged', function emulatorSetColor(x, y, color){
+    var el = document.getElementById('cell-'+ x +'-'+ y);
+
+    if (!el) {
+      return;
+    }
+
+    // Get rid of dark colors (because the floor is white)
+    // color = normalizeColors(color);
+
+    // Set color
+    el.style.background = 'rgb('+ color.join(',') +')';
+  });
 })();
