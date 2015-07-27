8f697cff8154ca3215c97525adee8a6c9ab98594
jgillick <j_gillick@yahoo.com>
2015-07-26 20:07:04 -0700

    Rename 'Running Primaries' to 'Primaries' and added a couple more modes

---
+++ Disco Controller/programs/primaries.js
@@ -0,0 +1,97 @@
+'use strict';
+
+var Promise = require("bluebird"),
+    discoUtils = require('../lib/utils.js');
+
+var floorController = null,
+    running = true,
+    colorSelect = [],
+    globalSelect = 0,
+    offsState = 0,
+    fadeDuration = 500,
+    timeout, modeTimeout,
+    mode = 'running';
+
+module.exports = {
+
+  info: {
+    name: 'Primaries',
+    description: 'Fades in primary colors, chasing from one cell to the next',
+    interactive: false,
+    miniumumTime: 0.5
+  },
+
+  /**
+    Setup the program
+  */
+  init: function(controller){
+    running = true;
+    floorController = controller;
+    return Promise.resolve();
+  },
+
+  /**
+    Shutdown this program and clear memory
+  */
+  shutdown: function(){
+    running = false;
+    clearTimeout(timeout);
+    return floorController.changeAllCells([0,0,0], 500);
+  },
+
+  /**
+    Run the program
+  */
+  run: function(){
+    if (!running) return;
+
+    modeTimeout = setInterval(function(){
+      switch(mode) {
+        case 'running':
+          mode = 'offs';
+          break;
+        case 'offs':
+          mode = 'all';
+          break;
+        case 'all':
+          mode = 'running';
+          break;
+      }
+    }, 6000);
+
+    program();
+  }
+
+};
+
+function program() {
+  var cells = floorController.getCells(),
+      rgb, color;
+
+  offsState = (offsState) ? 0 : 1;
+  globalSelect = discoUtils.wrap(globalSelect + 1, 0, 2);
+
+  for (var i = 0, last = 0, len = cells.length; i < len; i++) {
+    color = [0,0,0];
+    rgb = colorSelect[i] || last;
+
+    if (mode == 'running') {
+      rgb = discoUtils.wrap(rgb + 1, 0, 2);
+      last = rgb;
+      colorSelect[i] = rgb;
+
+      color[rgb] = 255;
+    }
+    else if (mode == 'offs' && offsState){
+      color[rgb] = 255;
+    }
+    else if (mode == 'all') {
+      color[globalSelect] = 255;
+    }
+
+    cells[i].fadeToColor(color, fadeDuration);
+    // cells[i].setColor(color);
+  }
+
+  timeout = setTimeout(program, fadeDuration * 1.5);
+}
