17c42769bad8bedb105e324afa661ea4ffc36a54
jgillick <j_gillick@yahoo.com>
2015-08-12 03:18:38 -0700

    Move SASS compilation to load with app

--- DiscoController/app.js
+++ DiscoController/app.js
@@ -1,18 +1,1 @@
 'use strict';
-
-// Compile SASS files
-var exec = require('child_process').exec;
-
-(function compileSass() {
-
-	var sassCompiler = exec('sass -I ./ --update ./scss:./public/stylesheets', function (error, stdout, stderr) {
-		if (stdout !== '') console.log('SASS: ' + stdout);
-		if (stderr !== '') console.log('SASS ERROR: ' + stderr);
-		if (error !== null) {
-			console.log('SASS exec error: ' + error);
-		}
-
-		// TODO: Refresh CSS files in the DOM
-	});
-
-})();
--- DiscoController/javascript/gui.js
+++ DiscoController/javascript/gui.js
@@ -1,6 +1,11 @@
-var gui = require('nw.gui');
+var gui = require('nw.gui'),
+	exec = require('child_process').exec;;
+
+
 win = gui.Window.get();
 var nativeMenuBar = new gui.Menu({ type: "menubar" });
+
+// console.log('Arguments!', gui.App.argv);
 
 // Add native keyboard shortcuts
 try {
@@ -9,3 +14,17 @@ try {
 } catch (ex) {
   console.log(ex.message);
 }
+
+// Compile SASS templates
+var sassCompiler = exec('sass -I ./ --update ./scss:./public/stylesheets', function (error, stdout, stderr) {
+	// if (stdout !== '') console.log('SASS: ' + stdout);
+	if (stderr !== '') console.log('SASS ERROR: ' + stderr);
+	if (error !== null) {
+		console.log('SASS exec error: ' + error);
+	}
+
+	// Reload stylesheets
+	$('link[rel=stylesheet]').each(function(){
+		this.setAttribute('href', this.href);
+	})
+});
