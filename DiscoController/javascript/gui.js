ed4c99f7d79224e6f77ab46e7402379b0f81569b
jgillick <j_gillick@yahoo.com>
2015-09-09 13:40:51 -0700

    Add support for command line options.

--- DiscoController/javascript/gui.js
+++ DiscoController/javascript/gui.js
@@ -1,13 +1,56 @@
+'use strict';
+
 var gui = require('nw.gui'),
     exec = require('child_process').exec,
     comm = require('./lib/comm.js'),
-    audio = require('./lib/audio.js');
+    audio = require('./lib/audio.js'),
+    yargs = require('yargs'),
+    fs = require('fs');
 
 
 var win = gui.Window.get(),
     nativeMenuBar = new gui.Menu({ type: 'menubar' });
 
-// console.log('Arguments!', gui.App.argv);
+// Parse arguments
+var argv = yargs.usage('DiscoController [options]')
+  .option('h', {
+        alias: 'help',
+        demand: false,
+        default: false,
+        describe: 'Display this message',
+        type: 'boolean'
+    })
+  .option('c', {
+        alias: 'config',
+        demand: false,
+        default: false,
+        describe: 'A JSON config file used to auto-start the floor and programs',
+        type: 'string'
+    })
+  .parse(gui.App.argv);
+
+// Show help
+if (argv.help) {
+  process.stdout.write(yargs.help() +'\n\n');
+  this.close();
+}
+
+// Parse config file
+if (argv.config !== false) {
+
+  if (argv.config.length == 0) {
+    process.stderr.write('ERROR: Invalid config file path\n');
+    this.close();
+  }
+  else {
+    try {
+      global.startupConfig = JSON.parse(fs.readFileSync(argv.config, 'utf8'));
+    } catch(e) {
+      process.stderr.write('ERROR: Could not process config file: '+ e.message +'\n');
+      this.close();
+    }
+  }
+}
 
 // Add native keyboard shortcuts
 try {
@@ -24,6 +67,11 @@ win.on('close', function(){
       this.close(true);
     }.bind(this));
   }.bind(this));
+
+  // Force close after a couple seconds
+  setTimeout(function(){
+    this.close(true);
+  }.bind(this), 1000);
 });
 
 // Compile SASS templates
