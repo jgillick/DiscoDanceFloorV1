b861f09c6d5cffa3c9351dc5e1ebdb8559f4b64c
jgillick <j_gillick@yahoo.com>
2015-09-09 13:41:21 -0700

    Put audio connection in a promise

--- DiscoController/lib/audio.js
+++ DiscoController/lib/audio.js
@@ -1,3 +1,4 @@
+'use strict';
 
 /**
   Provides helper functions/properties to the web audio API.
@@ -20,21 +21,25 @@ module.exports.init = function(navigator){
   analyser = audioCtx.createAnalyser();
 
   // Connect to audio source
-  navigator.webkitGetUserMedia(
-    {audio: true},
-    function(stream){
-      source = audioCtx.createMediaStreamSource(stream);
-      source.connect(analyser);
-      analyser.fftSize = 128;
+  return new Promise(function(resolve) {
+    navigator.webkitGetUserMedia(
+      {audio: true},
+      function(stream){
+        source = audioCtx.createMediaStreamSource(stream);
+        source.connect(analyser);
+        analyser.fftSize = 128;
 
-      module.exports.context = audioCtx;
-      module.exports.analyser = analyser;
-      module.exports.source = source;
-    },
-    function(e){
-      console.log('Could not connect to audio source: '+ e.message);
-    }
-  );
+        module.exports.context = audioCtx;
+        module.exports.analyser = analyser;
+        module.exports.source = source;
+        resolve();
+      },
+      function(e){
+        console.log('Could not connect to audio source: '+ e.message);
+        resolve();
+      }
+    );
+  });
 };
 
 /**
@@ -52,6 +57,23 @@ module.exports.close = function() {
       resolve();
     }
   }.bind(this));
+};
+
+/**
+  Returns the current audio samples reduced down
+  to a specific number of samples
+*/
+module.exports.getDataChunks = function(chunks) {
+  function median(values) {
+    if (!values.length) return 0;
+
+    values.sort( function(a,b) {return a - b;} );
+    var half = Math.floor(values.length/2);
+    if(values.length % 2)
+      return values[half];
+    else
+      return (values[half-1] + values[half]) / 2.0;
+  }
 };
 
 
